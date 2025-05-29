7839c50f630c518bf0021de8bb12b38ee5b78708
Pavan Sylesh <102213775+Sylesh01@users.noreply.github.com>
2025-05-29 18:12:31 +0530

    Update Assignment.py

--- Assignment.py
+++ Assignment.py
@@ -20,8 +20,6 @@ for i in dbutils.fs.ls("./FileStore/tables"):
     file_format = i.name.split(".")[-1]
     table_name = i.name.split(".")[0]
     df = spark.read.format(file_format)\
-        .option("cloudFiles.format", file_format)\
-        .option("cloudFiles.schemaLocation", i.path)\
         .load(i.path)
     df.write.mode("append").option("mergeSchema","True").saveAsTable(f"bronze.{table_name}")
 
