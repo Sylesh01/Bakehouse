f0f8da1592cc52718bd819ce203028e901615694
Pavan Sylesh <102213775+Sylesh01@users.noreply.github.com>
2025-05-29 18:41:24 +0530

    update code

--- Assignment.py
+++ Assignment.py
@@ -7,27 +7,25 @@
 
 # COMMAND ----------
 
-# DBTITLE 1,Creating tables
-for i in dbutils.fs.ls("./FileStore/tables"):
-    table_name = i.name.split(".")[0]
-    spark.sql(f"create table if not exists bronze.{table_name}")
-
-# COMMAND ----------
-
 # DBTITLE 1,Creating Bronze tables
-from pyspark.sql.functions import split
+from pyspark.sql.functions import split,lit
+from datetime import datetime
+
+cur_time = str(datetime.now())
 for i in dbutils.fs.ls("./FileStore/tables"):
     file_format = i.name.split(".")[-1]
     table_name = i.name.split(".")[0]
     df = spark.read.format(file_format)\
         .load(i.path)
+    df = df.withColumn("inserttime",lit(cur_time))
     df.write.mode("append").option("mergeSchema","True").saveAsTable(f"bronze.{table_name}")
 
 
 # COMMAND ----------
 
 # DBTITLE 1,creating silver tables
-from delta.tables import *
+from delta.tables import DeltaTable
+from pyspark.sql.functions import col
 tbls = spark.sql("Show Tables in bronze")
 tbls = tbls.where("isTemporary != true")
 tbls_lst = [i[0] for i in tbls.select("tableName").collect()]
@@ -39,17 +37,15 @@ primary_keys = {"media_customer_reviews":"new_id",
                 "sales_transactions":"transactionID"}
 
 for i in tbls_lst:
-    pk = primary_keys[i]
+    pk_str = primary_keys[i]
+    brnz_df = DeltaTable.forName(spark,f"bronze.{i}")
     brnz_df = spark.read.table(f"bronze.{i}")
-    brnz_df.createOrReplaceTempView("brnz_tbl")
     tbl_exist = spark.catalog.tableExists(f"silver.{i}")
     if tbl_exist:
-        sil_df = spark.read.table(f"silver.{i}")
-        sil_df.createOrReplaceTempView("sil_tbl")
-        spark.sql(f"""merge into sil_tbl using brnz_tbl on
-              sil_tbl.{pk} == brnz_tbl.{pk}
-              when matched then update set *
-              when not matched then insert *""" )
+        sil_df = DeltaTable.forName(spark,f"silver.{i}")
+        brnz_df = brnz_df.filter(col("inserttime") == cur_time).dropDuplicates(pk_str.split(","))
+        (sil_df.alias("tgt").merge(brnz_df.alias("src"),f"tgt.{pk_str} == src.{pk_str}")
+        .whenMatchedUpdateAll().whenNotMatchedInsertAll().execute())
     else:
         brnz_df.write.mode("append").saveAsTable(f"silver.{i}")
 
@@ -57,7 +53,7 @@ for i in tbls_lst:
 # COMMAND ----------
 
 # DBTITLE 1,Create Gold Tables
-df1 = spark.sql("select product,sum(quantity) as tot_prod_sold from silver.sales_transactions group by product order by tot_prod_sold desc")
+idf1 = spark.sql("select product,sum(quantity) as tot_prod_sold from silver.sales_transactions group by product order by tot_prod_sold desc")
 display(df1)
 df1.write.mode("overwrite").saveAsTable("gold.High_demand_table")
 
@@ -69,4 +65,8 @@ df2.write.mode("overwrite").saveAsTable("gold.franchises_supplied")
 df3 = spark.sql("""select sum(totalPrice) as sales, MONTH(dateTime) as Month from silver.sales_transactions group by Month order by sales desc""")
 display(df3)
 df3.write.mode("overwrite").saveAsTable("gold.Total_sales_per_month")
+
+
+
+# COMMAND ----------
 
